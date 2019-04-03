12f20414f926cae9f2ad66b4e38270773d0408e3
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-04-03 12:28:56 +0100

    add test for get and add entry methods

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -4,12 +4,24 @@ secret_diary = SecretDiary.new
 
 describe "lock diary method" do
   it "locks the diary" do
-  expect(secret_diary.lock).to eq "Diary is locked"
+  expect(secret_diary.lock).to eq "Diary is now locked"
   end
 end
 
 describe "unlock diary method" do
   it "unlocks the diary" do
-  expect(secret_diary.unlock).to eq "Diary is unlocked"
+  expect(secret_diary.unlock).to eq "Diary is now unlocked"
+  end
+end
+
+describe "add entry method" do
+  it "adds an entry" do
+  expect(secret_diary.add_entry).to eq "Entry added"
+  end
+end
+
+describe "get entry method" do
+  it "retrieves an entry" do
+  expect(secret_diary.get_entries).to eq "Entry retrieved"
   end
 end
